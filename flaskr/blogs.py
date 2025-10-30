70cd4d01b2ea4a19e4552bbfd731ca0317663bad
FukaOno <atopfuka@gmail.com>
2025-10-30 10:04:35 -0400

    事前課題提出” ”

--- flaskr/blogs.py
+++ flaskr/blogs.py
@@ -1,15 +1,15 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash # <= ここを追加
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flaskr import db
 # models.pyのBlogクラスをインポート
 from flaskr.models import Blog
 
-blog_bp = Blueprint('blogs', __name__, url_prefix='/blogs') # <= ここを変更
+blog_bp = Blueprint('blogs', __name__, url_prefix='/blogs')
 
 # 一覧表示
 @blog_bp.route('/')
 def index():
     blogs = Blog.query.order_by(Blog.created_at.desc()).all()
-    return render_template('blogs/index.html', blogs=blogs) # <= テンプレートパスを変更を追加
+    return render_template('blogs/index.html', blogs=blogs)
 
 # 新規投稿フォーム表示と投稿処理
 @blog_bp.route('/new', methods=['GET', 'POST'])
@@ -21,6 +21,15 @@ def create():
 
         # Blogインスタンスを作成
         new_blog = Blog(title=title, body=body, user_name=user_name)
+        # バリデーションを実行
+        errors = new_blog.validate()
+
+        # エラーがあればフォームに戻して表示する
+        if errors:
+            # エラーのリストの要素一つずつをflushに入れている
+            for e in errors:
+                flash(e, 'error')
+            return render_template('blogs/new.html', title=title, body=body, user_name=user_name)
         # DBへ保存
         db.session.add(new_blog)
         db.session.commit()
@@ -28,9 +37,10 @@ def create():
         # 保存後、詳細ページへリダイレクト
         return redirect(url_for('blogs.detail', blog_id=new_blog.id))
 
-    #blogs/new.htmlをテンプレートとしてHTMLを組み立てる
+    # blogs/new.htmlをテンプレートとしてHTMLを組み立てる
     return render_template('blogs/new.html')
 
+# 詳細ページ
 @blog_bp.route('/<int:blog_id>')
 def detail(blog_id):
     blog = Blog.query.get_or_404(blog_id)
